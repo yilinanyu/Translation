2e049a5f9f29cb601330f818addd5b4d3c9f195b
ylnnyu <ly783@nyu.edu>
2017-01-04 15:17:37 -0800

    something

---
+++ src/scripts/containers/apiresult.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import map from 'lodash/map'
+import { fetchAPIResult } from '../actions/index'
+
+
+class APIResult extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    // componentWillMount() {
+    //     this.props.checkTranslation()
+    // }
+
+    render() {
+        return (
+            <div> hssdasdas</div>
+           // <div className="panel panel-default">
+           //     <div className="panel-heading">{this.props.result.title}</div>
+           //     <div className="panel-body">
+           //         {this.getSelectedResult(this.props.result.selectedOptions)}
+           //     </div>
+           // </div>
+        )
+    }
+
+    // getSelectedResult(options) {
+    //     return map(options, option => {
+    //         return(
+    //             <div key={`selected-result-${option.id}`} className="options">
+    //                 <h6  className="title">{option.title}</h6>
+    //                 {this.getSelected(option.selected)}
+    //             </div>
+
+    //         )
+    //     })
+    // }
+
+    // getSelected(options) {
+    //     return map(options, option => {
+    //         return(
+    //             <div key={`selected-${option.id}`}  className={option.value} >
+    //                 <span>{option.label} : {option.value}</span>
+    //             </div>
+    //         )
+    //     })
+    // }
+
+}
+
+// result from reducer/index
+function mapStateToProps(appState){
+    return {APIResult: appState.APIResult}
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchAPIResult }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(APIResult)
---
+++ src/scripts/reducers/reducer_api.js
@@ -0,0 +1,12 @@
+import { FETCH_API_RESULT } from '../actions/index'
+// state argument is not application state, only the state this render is responsible for
+export default function(state=[], action) {
+    // do care about the actions
+    switch(action.type) {
+        // if the action type is SET_RESULT, we will return the action payload, which is the selected item.
+        case FETCH_API_RESULT:
+            return Object.assign({}, action.payload)
+    }
+    // action state is anything else, just return state.
+    return state;
+}
